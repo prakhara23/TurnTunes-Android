390ee9b53946055208cc617a4dcd0e255922cf4f
Prakhar <prakhar@clausehound.com>
2016-03-10 21:49:42 -0500

    changed the icon of the app

--- app/src/main/java/com/prakhara/turntunes/Song.java
+++ app/src/main/java/com/prakhara/turntunes/Song.java
@@ -5,6 +5,7 @@ public class Song {
     private String song;
     private String img;
     private String url;
+    private int song_id;
 
     //Empty for the purposes of Firebase
     public Song() { }
@@ -22,6 +23,10 @@ public class Song {
         return url;
     }
 
+    public int getSong_id() {
+        return song_id;
+    }
+
     //Setter Methods
     public void setSong(String songName) {
         song = songName;
@@ -33,5 +38,9 @@ public class Song {
 
     public void setUrl(String songUrl) {
         url = songUrl;
+    }
+
+    public void setSong_id(int songId) {
+        song_id = songId;
     }
 }
